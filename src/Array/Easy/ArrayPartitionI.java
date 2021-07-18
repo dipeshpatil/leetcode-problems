96e70f9218d8d92dfc0218a06716f151e07d9916
Dipesh Patil <heydipesh28@gmail.com>
2021-07-18 07:57:12 +0530

    sample program to test workflow take_2

--- src/Array/Easy/ArrayPartitionI.java
+++ src/Array/Easy/ArrayPartitionI.java
@@ -19,7 +19,6 @@ public class ArrayPartitionI {
 
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-
         int ans = 0;
         for (int i = 0; i < nums.length; i += 2)
             ans += nums[i];
